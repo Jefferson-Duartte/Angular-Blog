9fc333b3f6079bab0c585f08e002e6148d13c0d0
Jefferson Duarte <duartejefferson123@gmail.com>
2023-10-01 22:25:46 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -3,18 +3,19 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css'],
 })
 export class SmallCardComponent implements OnInit {
   @Input()
-  photoCover:string = ""
+  photoCover: string = '';
   @Input()
-  cardTitle:string = ""
+  cardTitle: string = '';
   @Input()
-  cardDescription:string = ""
+  cardDescription: string = '';
+  @Input()
+  id: string = '0';
+
   constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,34 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Sinopse do Homen de Ferro 1',
+    description:
+      "Tony Stark (Robert Downey Jr.) é um industrial bilionário, que também é um brilhante inventor. Ao ser sequestrado ele é obrigado por terroristas a construir uma arma devastadora mas, ao invés disto, constrói uma armadura de alta tecnologia que permite que fuja de seu cativeiro. A partir de então ele passa a usá-la para combater o crime, sob o alter-ego do Homem de Ferro.Homem de Ferro é um filme de super-herói de 2008 baseado no super-herói da Marvel Comics de mesmo nome.É o primeiro filme principal do Universo Cinematográfico Marvel, da Fase Um e da Saga do Infinito. Foi lançado em 30 de abril de 2008 internacionalmente e em 2 de maio de 2008 nos Estados Unidos.Foi dirigido por Jon Favreau e estrelado por Robert Downey, Jr. como Tony Stark, Terrence Howard como Tenente Coronel James 'Rhodey' Rhodes, Jeff Bridges como Obadiah Stane, Shaun Toub como Ho Yinsen e Gwyneth Paltrow como Pepper Potts.O filme deu origem a duas sequências: Homem de Ferro 2 em 2010 e Homem de Ferro 3 em 2013.",
+    photoCover:
+      'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AD72115B9CDAE873B2234BD0908CEF7B45A2D9AD9C66C699239B46E582F70A8B/scale?width=1200&aspectRatio=1.78&format=jpeg',
+  },
+  {
+    id: '2',
+    photoCover:
+      'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/0A2C59BE2EF8E3C95BD6A69AE050198F4E44123D4B5C070DA3F62EE076F4AD92/scale?width=1200&aspectRatio=1.78&format=jpeg',
+    title: 'O que acontece em Homen de Ferro 2?',
+    description:
+      'Homem de Ferro 2 é um filme de super-herói de 2010 baseado no super-herói Homem de Ferro.É uma sequência de Homem de Ferro, o terceiro filme principal do Universo Cinematográfico Marvel, da Fase Um e da Saga do Infinito.Foi lançado em 28 de abril de 2010 internacionalmente e em 7 de maio de 2010 nos EUA.Jon Favreau retorna como diretor e intérprete de Happy Hogan. Robert Downey, Jr., Gwyneth Paltrow e Samuel L. Jackson retornam em seus papéis como Tony Stark, Pepper Potts e Nick Fury, Don Cheadle substitui Terrence Howard como James Rhodes/Máquina de Combate, o filme apresenta Scarlett Johansson, Sam Rockwell e Mickey Rourke como Natasha Romanoff, Justin Hammer e Ivan Vanko.',
+  },
+  {
+    id: '3',
+    photoCover:
+      'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C0BA59D770EDF31BD282925F3C7D69A0A9F404CBBC44BE12A7BB310412C42FC7/scale?width=1200&aspectRatio=1.78&format=jpeg',
+    title: 'Relembre os melhores momentos de Homen de Ferro 3',
+    description:
+      'Me devolve o uniforme.Tony Stark pode começado sua trajetória no MCU como um bilionário mimado que só queria vender armas para o exército, mas tudo o que ele passou fez com que ele se transformasse em alguém melhor e que se preocupava com o próximo.A prova disso aparece em Homem-Aranha: De Volta ao Lar, filme em que Tony serve de mentor para o jovem Peter Parker. Parker estava deslumbrado com a vida de super-herói e queria a todo custo ser um Vingador, mas nesse processo, estava sendo imprudente e relapso com seus atos e Tony teve de ensinar a ele uma valiosa lição.',
+  },
+  {
+    id: '4',
+    photoCover:
+      'https://proxy.olhardigital.com.br/wp-content/uploads/2023/09/imagem_2023-09-28_165351201.png',
+    title: 'Homem de Ferro 4’: Vai mesmo ter o novo filme?',
+    description:
+      'Intérprete de Tony Stark entre 2008 e 2019, Robert Downey Jr. foi o rosto do Universo Cinematográfico da Marvel por quase 12 anos, desde ‘Homem de Ferro’ até o lançamento de ‘Vingadores: Ultimato’.E, apesar do personagem ter se sacrificado para salvar o universo, os fãs ainda esperam revê-lo em algum momento.',
+  },
+];
